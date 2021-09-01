0bca8beede7acfee93ad03fb21f7f50c0091dce8
Indu <indurevathy@gmail.com>
2021-09-01 17:04:20 +0200

    updated

--- tableReader.js
+++ tableReader.js
@@ -5,8 +5,11 @@
 
 // Variablen, die zwischengespeichert werden um hier im Script damit arbeiten zu können.
 let persons = [];
+let personssorted = [];
 const dispBtn1 = document.getElementById("dispBtn1");
+const dispBtn2 = document.getElementById("dispBtn2");
 const ul1 = document.getElementById("ul1"); 
+const ol1 = document.getElementById("ol1");
 
 // --------------------------------------------------------------------------------------
 
@@ -14,8 +17,11 @@ const ul1 = document.getElementById("ul1");
 dispBtn1.addEventListener("click", showList);
 pushNames();
 printToConsole(persons);
+//printToConsole(personssorted);
 
 // --------------------------------------------------------------------------------------
+//dispBtn2.addEventListener("click", showList);
+
 
 
 // Implementierungen der einzelnen Funktionen.
@@ -23,46 +29,43 @@ printToConsole(persons);
 
 // showList: Namen aus dem "persons"-Array der oben deklariert wurde, werden als Listenelemente einem anderen Listenelement untergeordnet und deren Text wird gleichgesetzt
 // mit dem Namen der Person an Index i.
+//Zwei verschiedene Elemente hinzugefügt, eines für eine geordnete Liste und das andere für eine ungeordnete Liste.
 function showList() {
-
+    
     for (let i = 0; i < persons.length; i++){
+        let newOrderedListElement = document.createElement("li");
+        let newUnorderedListElement = document.createElement("li");
 
-        let newListElement = document.createElement("li");
-        newListElement.innerText = persons[i].firstName + " " + persons[i].lastName;
-        ul1.appendChild(newListElement);
+        newOrderedListElement.innerText = persons[i].firstName + " " + persons[i].lastName;
+        newUnorderedListElement.innerText = persons[i].firstName + " " + persons[i].lastName;
+
+        ol1.appendChild(newOrderedListElement);
+        ul1.appendChild(newUnorderedListElement);
+        
     }
 }
 
+
+    
+    
 // pushNames: Es werden Objekte mit zwei internen Variablen "firstName" und "lastName" erstellt. Die Wertzuweisungen erfolgen ebenfalls auf direktem Wege. Danach werden die
 // Objekte in das Array "persons" gepusht.
+function pushName(fName, lName) {
+    persons.push({
+        firstName: fName,
+        lastName: lName
+    });
+    }
 function pushNames (){
 
-    const person = {
-        firstName: "Anton",
-        lastName: "Mustermann"
-    }
-    
-    persons.push(person);
-    
-    persons.push({
-        firstName: "Moritz",
-        lastName: "Mustermann"
-    });
-    
-    persons.push({
-        firstName: "Berta",
-        lastName: "Bertelsmann"
-    });
-  
-  persons.push({
-    firstName: "Julius",
-    lastName: "Cäsar"
-});
-  
+    pushName("Anton","Mustermann");
+    pushName("Moritz","Mustermann");
+    pushName("Berta","Bertelsmann");
+    pushName("Julius","Cäsar");
 }
-
 
 // Simple Ausgabe in die Konsole.
 function printToConsole(outputStr) {
     console.log(outputStr);
 }
+
